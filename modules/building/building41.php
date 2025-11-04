000a84016fcab6f228ec362ecefc85101adafe91
Victoria La <laskava@gmail.com>
2025-11-03 21:15:53 -0500

    Bug #87602: "Construction of CRANE did not allow additional build action"

--- modules/building/building41.php
+++ modules/building/building41.php
@@ -1,5 +1,7 @@
 <?php 
 
+//         	41 : {"name":_("Crane"), "most":false, "per":false, "tooltip":_("Immediately place another Worker in the Guildhall to either construct another Building or advance work on the Cathedral.") }, 
+       
 class building41 extends building
 {
     public $vp = 3;
@@ -7,28 +9,30 @@ class building41 extends building
     
     public function instant($player)
     {
-        ;
         $arg = $player->argguildhall(null,null);
-        if(count($arg['selectable'])>1)
+        if(count($arg['selectable'])>0)
         {
             $worker_id = self::getUniqueValueFromDB( "select min(id) from worker where player_id = {$player->player_id} and location like 'reserve%'");
-            self::DbQuery("update worker set location = 'guildhall', location_arg=0 where id = {$worker_id}");
-            $worker = self::getObjectFromDB("SELECT * FROM worker WHERE id = {$worker_id}");
-            ArchitectsOfTheWestKingdom::$instance->notifyAllPlayers( "move", '', array(
-                'mobile' => "worker_".$worker['id'],
-                'parent' => $worker['location'],
-                'position' => 'last'
-            ) ); 
-            $nbmeeplesLeft = self::getUniqueValueFromDB( "select count(*) from worker where player_id = {$player->player_id} and  location like 'reserve%'");
-            ArchitectsOfTheWestKingdom::$instance->notifyAllPlayers( "counter", clienttranslate('${player_name} places a worker on ${location}'), array(
-                'player_id' => $player->player_id,
-                'player_name' => $player->player_name,
-                'id' => "res_".$player->player_id."_8",
-                'nb' => $nbmeeplesLeft,
-                'location' => "guildhall"
-            ) );
-            
-            $player->guildhallSelect($worker_id);
+            if($worker_id != null)
+            {
+                self::DbQuery("update worker set location = 'guildhall', location_arg=0 where id = {$worker_id}");
+                $worker = self::getObjectFromDB("SELECT * FROM worker WHERE id = {$worker_id}");
+                ArchitectsOfTheWestKingdom::$instance->notifyAllPlayers( "move", '', array(
+                    'mobile' => "worker_".$worker['id'],
+                    'parent' => $worker['location'],
+                    'position' => 'last'
+                ) ); 
+                $nbmeeplesLeft = self::getUniqueValueFromDB( "select count(*) from worker where player_id = {$player->player_id} and  location like 'reserve%'");
+                ArchitectsOfTheWestKingdom::$instance->notifyAllPlayers( "counter", clienttranslate('${player_name} places a worker on ${location}'), array(
+                    'player_id' => $player->player_id,
+                    'player_name' => $player->player_name,
+                    'id' => "res_".$player->player_id."_8",
+                    'nb' => $nbmeeplesLeft,
+                    'location' => "guildhall"
+                ) );
+                
+                $player->guildhallSelect($worker_id);
+            }
         }
     }
 }
